b629fef3603a899b5bdcdedbefb99f451cda033b
Pradnya Sadhu Putra <pradnya.putra@gmail.com>
2020-09-03 16:21:45 -0400

    added 404 validation

--- Main.py
+++ Main.py
@@ -5,17 +5,26 @@ from bs4 import BeautifulSoup
 def reviewScrape():
     preset = "https://pitchfork.com/reviews/albums/"
     query = "rich-brian-the-sailor"
-
     search = preset + query + "/"
 
     source = requests.get(search).text
-
     soup = BeautifulSoup(source, 'lxml')
+    errorFinding = soup.find("div", class_="error-page__heading")
 
-    review =""
-    for paragraph in soup.find_all("p"):
-        review = review+"\n"+paragraph.text
-    print(review)
+    try:
+        if(errorFinding.text)=="404":
+            print("Sorry, a review can't be found on this album!")
+    except:
+        title = soup.title.text
+
+        review = ""
+        for paragraph in soup.find_all("p"):
+            if review == "":
+                review = paragraph.text
+            else:
+                review = review+"\n\n"+paragraph.text
+        print(title)
+        print(review)
 
 
 reviewScrape()
