cab138a3dd1037c8289d63d36796ce03e76c7e83
Pradnya Sadhu Putra <pradnya.putra@gmail.com>
2020-09-07 23:13:30 -0400

    webscraped reviews added to GUI

--- pythonFiles/secrets.py
+++ pythonFiles/secrets.py
@@ -1,4 +1,4 @@
-spotifyToken="BQBgLi4u42-H8k9SxFu80YLITdtJif-rVQr2CGtXPkvkdRSWa1Uvfavd6dJ7nrKTZq8HgUR83ogCgs1wFHuAQcWzdrAq5fUyDBXR1j-_tunFRyxnfJ7lOKNBW0XzA3OwA54qIJWDC4ZKaLPDBUAuJ2w"
+spotifyToken=""
 
 #To get an access token, visit the link and select "user-read-currently-playing" as the scope.
 #Then copy and paste the generated access token into the variable initialization above
